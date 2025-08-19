1ee71f8cd4910c72be183f766b2b3ee8e6c264bf
Vansh <vanshchavda711@gmail.com>
2025-08-20 03:34:07 +0530

    just a checkpoint before adding H$

--- request_a_ride.java
+++ request_a_ride.java
@@ -24,7 +24,14 @@ public class request_a_ride extends Thread{
         check_route_thread.setDaemon(true);//ensures that this new thread also ends with the end of main thread.
         check_route_thread.start();
 
-        Thread.sleep(4000);
+        //by the we wait here
+        //we can start the new thread that will create a bst from cities name.
+
+        //this will create the bst in background
+
+
+        //may have to make a change here
+        check_route_thread.join();
 
         System.out.println("Route service starting... Check your path now");
         System.out.println("Press enter when you have checked the path : ");
@@ -39,37 +46,5 @@ public class request_a_ride extends Thread{
                 "http://localhost:8080/actuator/shutdown").start();
         Thread.sleep(2000);
         System.out.println("Proceeding ahead");
-
-
-        //2. driver search starts
-        BufferedReader user_location_br = new BufferedReader(new FileReader("pick_up_coordinates.txt"));
-        String user_location = user_location_br.readLine();
-        user_location.replace("%2C" , ",");
-        String[] user_location_arr = user_location.split(",");
-
-
-        //now we will convert user location into geohash code
-        Double user_location_lat = Double.parseDouble(user_location_arr[0]);
-        Double user_location_lng = Double.parseDouble(user_location_arr[1]);
-
-        String user_location_geohash = GeoHash.geoHashStringWithCharacterPrecision(user_location_lat,user_location_lng,6);
-        System.out.println("Geohash: " + user_location_geohash);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
